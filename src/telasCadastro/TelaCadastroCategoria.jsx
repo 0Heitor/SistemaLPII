e3c67becd78eca2767b0477730566c0dad4cb8d9
0Heitor <heitortanizaka@terra.com.br>
2023-10-07 23:12:21 -0300

    categoria

--- src/telasCadastro/TelaCadastroCategoria.jsx
+++ src/telasCadastro/TelaCadastroCategoria.jsx
@@ -1,20 +1,56 @@
 import { Container } from "react-bootstrap";
+import { useState } from "react";
 import Pagina from "../templates/Pagina";
 import FormCadCategoria  from "./formularios/FormCadCategorias";
 import TabelaCategoria from "./tabelas/TabelaCategorias";
-import { useState } from "react";
+import TelaMensagem from "./TelaMensagem";
 
 export default function TelaCadastroCategoria(props) {
     const [exibirFormulario, setExibirFormulario] = useState(false);
-    return (
-        <Container>
-            <Pagina>
-                {
-                    //dinâmica em que o usuário irá alternar entre o formulário de cadastro
-                    //e a visualização do registros já cadastrados.
-                    exibirFormulario ? <FormCadCategoria exibirFormulario={setExibirFormulario}/> : <TabelaCategoria exibirFormulario={setExibirFormulario} />
-                }
-            </Pagina>
-        </Container>
-    )
+    const [listaCategorias, setListaCategorias] = useState([]);
+    const [mostrarMensagem, setMostrarMensagem] = useState(false);
+    const [mensagem, setMensagem] = useState("");
+    const [tipoMensagem, setTipoMensagem] = useState("");
+    const [modoEdicao, setModoEdicao] = useState(false);
+    const [categoriaParaEdicao, setCategoriaParaEdicao] = useState({
+        codigo:'',
+        nome:'',
+        descricao:''
+    });
+    
+    if (mostrarMensagem) {
+        return (
+            <TelaMensagem mensagem={mensagem} tipo={tipoMensagem} setMostrarMensagem={setMostrarMensagem}/>
+        )
+    }
+    else{
+        return (
+            <Container>
+                <Pagina>
+                    {
+                        exibirFormulario ? <FormCadCategoria exibirFormulario={setExibirFormulario}
+                            listaCategorias={listaCategorias}
+                            setListaCategorias={setListaCategorias}
+                            categoriaParaEdicao={categoriaParaEdicao}
+                            setCategoriaParaEdicao={setCategoriaParaEdicao}
+                            modoEdicao={modoEdicao}
+                            setModoEdicao={setModoEdicao}
+                            setMostrarMensagem={setMostrarMensagem}
+                            setMensagem={setMensagem}
+                            setTipoMensagem={setTipoMensagem}
+                        />
+                            :
+                            <TabelaCategoria exibirFormulario={setExibirFormulario}
+                                listaCategorias={listaCategorias}
+                                setListaCategorias={setListaCategorias}
+                                categoriaParaEdicao={categoriaParaEdicao}
+                                setCategoriaParaEdicao={setCategoriaParaEdicao}
+                                modoEdicao={modoEdicao}
+                                setModoEdicao={setModoEdicao}
+                            />
+                    }
+                </Pagina>
+            </Container>
+        )
+    }
 }
--- src/telasCadastro/formularios/FormCadCategorias.jsx
+++ src/telasCadastro/formularios/FormCadCategorias.jsx
@@ -1,20 +1,72 @@
 import { Button, Container, Form, Row, Col, FloatingLabel } from "react-bootstrap";
+import { useState } from 'react';
+
 export default function FormCadCategoria(props) {
     //codigo
     //nome
     //descricao
+    const categoriaVazio = {
+        codigo:'',
+        nome:'',
+        descricao:''
+    }
+    const estadoInicialCategoria = props.categoriaParaEdicao;
+    const [categoria, setCategoria] = useState(estadoInicialCategoria);
+    const [formValidado, setFormValidado] = useState(false);
+
+    function manipularMudancas(e){
+        const componente = e.currentTarget;
+        setCategoria({...categoria,[componente.name]:componente.value});
+    }
+
+    function manipularSubmissao(e){
+        const form = e.currentTarget; 
+        if (form.checkValidity()){
+            if(!props.modoEdicao){
+                props.setListaCategorias([...props.listaCategorias,categoria]);
+                props.setMensagem('Categoria incluído com sucesso');
+                props.setTipoMensagem('success');
+                props.setMostrarMensagem(true);
+            }
+            else{
+                props.setListaCategorias([...props.listaCategorias.filter((itemCategoria)=>itemCategoria.codigo !== categoria.codigo),categoria]);
+                props.setModoEdicao(false);
+                props.setCategoriaParaEdicao(categoriaVazio);
+            }
+            setCategoria(categoriaVazio);
+            setFormValidado(false);
+        }
+        else{
+            setFormValidado(true);
+        }
+        e.stopPropagation();
+        e.preventDefault();
+    }
+
     return (
         <Container>
-            <Form>
+            <Form noValidate validated={formValidado} onSubmit={manipularSubmissao} >
                 <Row>
                     <Col>
                         <Form.Group>
                             <FloatingLabel
-                                controlId="floatingInput"
+                                label="Código:"
+                                className="mb-3"
+                            >
+                            <Form.Control type="text" placeholder="Informe o código da Categoria" id="codigo" name="codigo" value={categoria.codigo} onChange={manipularMudancas} required />
+                            </FloatingLabel>
+                            <Form.Control.Feedback type="invalid">Informe o codigo da categoria!</Form.Control.Feedback>
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Form.Group>
+                            <FloatingLabel
                                 label="Nome:"
                                 className="mb-3"
                             >
-                            <Form.Control type="text" placeholder="Informe o nome da Categoria" id="nome" name="nome" required />
+                            <Form.Control type="text" placeholder="Informe o nome da Categoria" id="nome" name="nome" value={categoria.nome} onChange={manipularMudancas} required />
                             </FloatingLabel>
                             <Form.Control.Feedback type="invalid">Informe o nome da categoria!</Form.Control.Feedback>
                         </Form.Group>
@@ -24,11 +76,10 @@ export default function FormCadCategoria(props) {
                     <Col>
                         <Form.Group>
                             <FloatingLabel
-                                controlId="floatingInput"
                                 label="Descricao:"
                                 className="mb-3"
                             >
-                            <Form.Control type="text" placeholder="Informe a descricao" id="descricao" name="descricao" required />
+                            <Form.Control type="text" placeholder="Informe a descricao" id="descricao" name="descricao" value={categoria.descricao} onChange={manipularMudancas} required />
                             </FloatingLabel>
                             <Form.Control.Feedback type="invalid">Informe a descricao!</Form.Control.Feedback>
                         </Form.Group>
@@ -36,11 +87,11 @@ export default function FormCadCategoria(props) {
                 </Row>
                 <Row>
                     <Col md={6} offset={5} className="d-flex justify-content-end">
-                        <Button type="submit" variant={"primary"}>Cadastrar</Button>
+                    <Button type="submit" variant={"primary"}>{props.modoEdicao ? "Alterar":"Cadastrar"}</Button>
                     </Col>
                     <Col md={6} offset={5}>
                         <Button type="button" variant={"secondary"} onClick={()=>{
-                            props.exibirFormulario(false)
+                            props.exibirFormulario(false);
                         }}>Voltar</Button>
                     </Col>
                 </Row>
